efe3116f082bfb20462dcdb8a92374c300cc352a
evencoding <evencoding@gmail.com>
2022-12-08 14:27:31 +0900

    feat: 게임 시작 메시지를 출력하는 기능 구현

--- src/controller/BridgeController.js
+++ src/controller/BridgeController.js
@@ -1,3 +1,23 @@
-class BridgeController {}
+const BridgeGame = require('../service/BridgeGame');
+const OutputView = require('../views/OutputView');
+
+class BridgeController {
+  #bridgeGame;
+
+  constructor() {
+    this.#bridgeGame = new BridgeGame();
+  }
+
+  gameStart() {
+    this.#noticeGameStart();
+    this.#inputBridgeSize();
+  }
+
+  #noticeGameStart() {
+    OutputView.printGameStartMessage();
+  }
+
+  #inputBridgeSize() {}
+}
 
 module.exports = BridgeController;
