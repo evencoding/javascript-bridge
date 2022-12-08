4d0d29a253e3b8d8fc6cd0a08bff588170419521
evencoding <evencoding@gmail.com>
2022-12-08 18:14:53 +0900

    refactor(Validator): 상수 제거

--- src/Validator.js
+++ src/Validator.js
@@ -1,26 +1,23 @@
+const { ERROR_MESSAGE, BRIDGE, REGEX } = require('./constants');
+
 const Validator = {
   getErrorMessageIfInvalidBridgeSize(bridgeSize) {
-    const regex = /^[1-9][0-9]*$/;
-    if (!regex.test(bridgeSize)) {
-      return '다리의 길이는 0으로 시작하지 않는 정수여야 합니다.';
-    }
-    if (bridgeSize < 3 || bridgeSize > 20) {
-      return '다리의 길이는 3부터 20 사이의 숫자여야 합니다.';
+    if (!REGEX.SIZE.test(bridgeSize)) return ERROR_MESSAGE.SIZE;
+    if (bridgeSize < BRIDGE.MIN_SIZE || bridgeSize > BRIDGE.MAX_SIZE) {
+      return ERROR_MESSAGE.SIZE_RANGE;
     }
 
     return '';
   },
 
   getErrorMessageIfInvalidDirection(direction) {
-    const regex = /^U|D$/;
-    if (!regex.test(direction)) return 'U와 D 중 하나의 값을 입력해주세요.';
+    if (!REGEX.DIRECTION.test(direction)) return ERROR_MESSAGE.DIRECTION;
 
     return '';
   },
 
   getErrorMessageIfInvalidCommand(command) {
-    const regex = /^R|Q$/;
-    if (!regex.test(command)) return 'R과 Q 중 하나의 값을 입력해주세요.';
+    if (!REGEX.COMMAND.test(command)) return ERROR_MESSAGE.COMMAND;
 
     return '';
   },
