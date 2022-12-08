07175c609c942d40b0f41acba8f5043ca22439d4
evencoding <evencoding@gmail.com>
2022-12-08 18:15:17 +0900

    test: 유효성 검사 테스트 구현

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,18 +1,27 @@
-test('getErrorMessageIfInvalidBridgeSize 테스트', () => {
-  const Validator = require('../src/Validator');
-  const errorMessage = Validator.getErrorMessageIfInvalidBridgeSize(0);
-  expect(errorMessage).toBe(
-    '다리의 길이는 0으로 시작하지 않는 정수여야 합니다.'
-  );
+const Validator = require('../src/Validator');
+
+const { ERROR_MESSAGE } = require('../src/constants');
+
+test.each([
+  ['2', ERROR_MESSAGE.SIZE_RANGE],
+  ['21', ERROR_MESSAGE.SIZE_RANGE],
+  ['03', ERROR_MESSAGE.SIZE],
+])('다리 사이즈 유효성 검사 테스트', (size, expect) => {
+  expect(Validator.getErrorMessageIfInvalidBridgeSize(size)).toBe(expect);
 });
 
-test.each([['2', ERROR], ['21'], ['03']])(
-  'getErrorMessageIfInvalidBridgeSize 테스트',
-  (size, expect) => {
-    const Validator = require('../src/Validator');
-    const errorMessage = Validator.getErrorMessageIfInvalidBridgeSize(0);
-    expect(errorMessage).toBe(
-      '다리의 길이는 0으로 시작하지 않는 정수여야 합니다.'
-    );
-  }
-);
+test.each([
+  ['U', ''],
+  ['D', ''],
+  ['A', ERROR_MESSAGE.DIRECTION],
+])('방향 유효성 검사 테스트', (direction, expect) => {
+  expect(Validator.getErrorMessageIfInvalidDirection(direction)).toBe(expect);
+});
+
+test.each([
+  ['R', ''],
+  ['Q', ''],
+  ['A', ERROR_MESSAGE.COMMAND],
+])('재시작 커맨드 유효성 검사 테스트', (command, expect) => {
+  expect(Validator.getErrorMessageIfInvalidCommand(command)).toBe(expect);
+});
